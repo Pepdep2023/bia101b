2887c84b0024e88261755ae81fc7bbcee89466b6
my_GitHub <03230228.gcbs@rub.edu.bt>
2024-03-01 12:02:35 +0600

    data type

---
+++ datatype.py
@@ -0,0 +1,8 @@
+var1 = 10  # Integer
+name = "Dechen" # String
+name2 = "Dechen2"
+score = 10.1 # float/decimals
+didHeWin = True # Boolean
+
+print(var1)
+print(type(name))
--- session1.py
+++ session1.py
@@ -1,1 +1,3 @@
 print("hello world")
+print("hello uncle")
+print("hello aunty")
