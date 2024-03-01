adbaafeb36775eb72a1b20909576c7faa8fd9300
my_GitHub <03230228.gcbs@rub.edu.bt>
2024-03-01 12:55:56 +0600

    Calculation

---
+++ conditionals.py
@@ -0,0 +1,30 @@
+# a calculator application made using terminal
+# variables and if statements
+
+# 1. get-input from user
+# 2. do calculation based on user-input
+# 3. give output to the user
+
+
+print('* multiplication')
+print('+ addition')
+print('- subtraction')
+print('/ division')
+
+whatUsertyped = input()
+ 
+print('User typed',whatUsertyped)
+print('user input type',type (whatUsertyped))
+
+if whatUsertyped == "+":
+    print('Doing addition')
+
+print('doing more addition')
+
+
+if whatUsertyped == "-":
+    print('doing subtraction')
+    print('doing more substraction')
+
+
+
