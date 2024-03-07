3c5e295910eb576b2cee1ccf2c47813383da6421
my_GitHub <03230228.gcbs@rub.edu.bt>
2024-03-07 15:10:48 +0600

    stringsCapitalize

---
+++ stringsEx.py
@@ -0,0 +1,7 @@
+# get user input
+# convert the user input into ALL upper case
+# print the converted strings
+
+userInput = input("Enter a string: ")
+userinputConverted = userInput.upper()
+print(userinputConverted)
