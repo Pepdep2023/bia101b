e8621b05b3ba1859950caffc2a2a01ea62e1a5d5
my_GitHub <03230228.gcbs@rub.edu.bt>
2024-03-14 15:20:39 +0600

    def fn

---
+++ functions.py
@@ -0,0 +1,27 @@
+def greet_nima():
+    #do calculation code
+    #complex stuffs
+    print("hello there")
+
+#to work, call a function like greet_nima()
+
+user_input = "Nima"
+
+if user_input == "Nima":
+    greet_nima()
+
+def greet():
+    # do some calculation code 
+    # complex stuffs
+    print('Hello')
+
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print('adding numbers')
+    return sum
+
+userNumber = 10
+userNumber2 = 20
+
+returnedSum = add_numbers(userNumber, userNumber2)
+print(returnedSum)
